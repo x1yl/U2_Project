402e5199ddc13eaa8105ca35f8ec93e8e1490557
x1yl <life23243@tutanota.com>
2025-11-17 07:30:21 -0500

    11/16 work

--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,48 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Game {
-    String playerName;
-    String difficulty;
-    public Game(String playerName, String difficulty) {
-        this.playerName = playerName;
+    private String playerName, difficulty;
+    private int health, maxHealth, attack, day;
+    private ArrayList<String> inventory;
+    private Scanner scanner;
+
+    public Game(String name, String difficulty, Scanner scanner) {
+        this.playerName = name;
         this.difficulty = difficulty;
+        this.scanner = scanner;
+
+        if (difficulty.equals("easy")) {
+            health = 25;
+            maxHealth = 25;
+            attack = 6;
+        } else if (difficulty.equals("medium")) {
+            health = 20;
+            maxHealth = 20;
+            attack = 5;
+        } else {
+            health = 15;
+            maxHealth = 15;
+            attack = 4;
+        }
+
+        day = 1;
+        inventory = new ArrayList<String>();
     }
 
     public void start() {
+        System.out.println("Welcome " + playerName + "! You have chosen " + difficulty + " mode. Best of luck and be not afraid.");
+        gameLoop();
+    }
 
+    private void gameLoop() {
+        //a while loop that runs for 20 days or whenever the player dies
+        //Shows the day and player info
+        //Option to explore or rest
+
+        // only if the player is alive after the 20th day spawn a boss battle
+
+        //Show player their stats after the game ends
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("What is your name? ");
         String name = scanner.nextLine();
+
+        //Repeatedly as the player for their chosen difficulty until their input is "easy, medium, or hard"
         String difficulty = "";
         while (!difficulty.equals("easy") && !difficulty.equals("medium") && !difficulty.equals("hard")) {
             System.out.print("Choose your difficulty (easy, medium, hard): ");
             difficulty = scanner.nextLine();
         }
-        Game game = new Game(name, difficulty);
+
+        Game game = new Game(name, difficulty, scanner);
+        game.start();
     }
 }
